d7e7879899a9e6c09f0b1415228af38cf3faf5cc
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-08-24 10:48:25 +0700

    feat(fish): implement fish endpoint

--- fetch/routers/router.py
+++ fetch/routers/router.py
@@ -1,5 +1,7 @@
+import routers.fish.routes as routes
+
 def register(app):
     '''
     register register all api route
     '''
-    pass
+    app.register_blueprint(routes.fish, url_prefix='/fish')
